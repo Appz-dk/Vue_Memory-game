d88e10c6b0fc2d227085a9c9cfa7a785a3164cf2
Appz-dk <patrick.fyhn@gmail.com>
2023-10-30 22:40:07 +0100

    Changed data to allow for card matching

--- src/data/cardData.ts
+++ src/data/cardData.ts
@@ -6,10 +6,18 @@ export type TCard = {
 
 export  const cardsData: TCard[] = []
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 8; i++) {
   cardsData.push({
     value: i,
     position: i,
+    isVisible: false
+  })
+}
+
+for (let i = 0; i < 8; i++) {
+  cardsData.push({
+    value: i,
+    position: i + 8,
     isVisible: false
   })
 }
