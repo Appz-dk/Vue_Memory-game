9de9579e8f9b9c702fca19ddc9df397e3c77dcdc
Appz-dk <patrick.fyhn@gmail.com>
2023-10-30 20:35:48 +0100

    Gameboard setup

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,42 @@
 </script>
 
 <template>
-  <h1>Vue Memory Game</h1>
-</template>
+    <h1>Vue Memory Game</h1>
+    <section class="game-board">
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+    </section>
+  </template>
 
 <style scoped>
 
+  h1 {
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+  .game-board {
+    display: grid;
+    grid-template-columns: repeat(4, 100px);
+    grid-template-rows: repeat(4, 100px);
+    gap: 1.5rem;
+    justify-content: center;
+  }
+
+  .card {
+    border: 3px solid #eee;
+  }
 </style>
